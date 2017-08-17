505ce758e923629ed4cb8e2e7fcb1a239d3c2a2d
rekha-sriramagiri <31059258+rekha-sriramagiri@users.noreply.github.com>
2017-08-17 20:43:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,14 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+
+// Move the image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 1;
+    ig.style.marginLeft = marginLeft + 'px';
+}
+img.oneclick = function () {
+    var interval = setInterval(moveRight, 50);
+};
